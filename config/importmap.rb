fbef6b93b70fd58f391353b3b99674ffc223f80f
hss-mateus <hss-mateus@pm.me>
2023-07-10 11:59:14 -0300

    Remove bootstrap

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,5 +2,3 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "popper", to: "popper.js", preload: true
-pin "bootstrap", to: "bootstrap.min.js", preload: true
